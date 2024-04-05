fd2e0a5d444df6b3b3be9b62ddd722a4849c3067
ak <ak8893893@yahoo.com.tw>
2024-04-05 12:28:02 +0800

    10-頁面之間的導航

--- 02-code/my-app/src/app/book/page.tsx
+++ 02-code/my-app/src/app/book/page.tsx
@@ -1,6 +1,20 @@
+import Link from "next/link";
 
 export default function Book() {
-  return (
-    <div>Book</div>
-  );
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/book">All Books</Link>
+                    </li>
+                    {/* 這邊如果有需要可以繼續添加其他連結 */}
+                </ul>
+            </nav>
+            <h1>Book</h1>
+        </div>
+    );
 }
--- 02-code/my-app/src/app/page.tsx
+++ 02-code/my-app/src/app/page.tsx
@@ -1,7 +1,27 @@
 import Image from "next/image";
 
+import Link from "next/link";
+
+import React from "react";
+
 export default function Home() {
   return (
-    <div>Hello Next.js</div>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/book">All Books</Link>
+          </li>
+          {/* 這邊如果有需要可以繼續添加其他連結 */}
+        </ul>
+      </nav>
+      <main>
+        {/* 這裡可以放首頁的內容 */}
+        <h1>歡迎來到首頁</h1>
+      </main>
+    </div>
   );
 }
